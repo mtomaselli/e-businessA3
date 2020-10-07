0a3f2c124ad7a7898b6f5ace33289bfb2cce45b3
hughie <s3723244@student.rmit.edu.au>
2020-10-07 22:07:32 +1100

    functional cart

--- js/simplecartjs-config.js
+++ js/simplecartjs-config.js
@@ -1,33 +1,4 @@
-simpleCart({
-cartStyle: "table", // Display cart contents as a table.
-currency: "AUD", // Use Australian dollars as the currency.
-shippingFlatRate: 5.95, // Set a flat shipping rate of $5.95.
-shippingQuantityRate: 1.00, // Set an additional shipping rate of $1.00 per product in cart
-taxRate: 0.1, // Set a tax rate to reflect the Australian GST rate of 10%.
-cartColumns: [
-// Add image to cart contents table
-{view: function(item, column){
-return "<img src='" + item.get('image') + "' class='product_image_thumbnail'>";
-},
-attr: "image", label: "Product Image"},
-{ attr: "name", label: "Name"},
-{ attr: "size", label: "Size"},
-{ view: "currency", attr: "price", label: "Price"},
-{ view: "decrement", label: false},
-{ attr: "quantity", label: "Qty"},
-{ view: "increment", label: false},
-{ view: "currency", attr: "total", label: "SubTotal" },
-{ view: "remove", text: "Remove", label: false}
-],
-
-checkout: {
-type: "SendForm" ,
-method: "POST", // Set to "GET" or "POST". Default is "POST".
-url: "http:/sat.csit.rmit.edu.au/∼e46281/inte2047/processFormData.php"
-},
-});
-
-simpleCart.bind( 'beforeCheckout' , function( data ){  
+simpleCart.bind( 'beforeCheckout' , function( data ) {  
 data.first_name = document.getElementById("fname").value;
 data.last_name = document.getElementById("lname").value;
 data.phone_number = document.getElementById("phone_number").value;
@@ -44,4 +15,19 @@ data.cc_amex = document.getElementById("cc_amex").checked;
 data.ccname = document.getElementById("credit_card_name").value;
 data.ccnumber = document.getElementById("credit_card_number").value;
 data.ccsecuritycode = document.getElementById("credit_card_security_code").value;
+})
+
+simpleCart ({
+    cartStyle: "table",
+    currency: "AUD",
+
+    cartColumns: [
+		{ attr: "name" , label: "Name" } ,
+		{ attr: "price" , label: "Price", view: 'currency' } ,
+		{ view: "decrement" , label: false , text: "-" } ,
+		{ attr: "quantity" , label: "Qty" } ,
+		{ view: "increment" , label: false , text: "+" } ,
+		{ attr: "total" , label: "SubTotal", view: 'currency' } ,
+		{ view: "remove" , text: "Remove" , label: false }
+	]
 });
